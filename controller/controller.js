f50bbabb69d21dac50afb0d6b6004bdead562e0a
Alexandre Furlan <alexfurlan1973@gmail.com>
2021-05-20 16:22:52 -0300

    Fazendo a pagina cadastro funcionar (error).

--- controller/controller.js
+++ controller/controller.js
@@ -11,8 +11,26 @@ module.exports.login = (req, res) => {
     });
 };
 
-module.exports.cadastrar = (req, res) => {
+module.exports.showCadastro = (req, res) => {
   res.render('cadastro', {
-    pagina: 'cadastro'
+    pagina: 'cadastro',
+    error: {},
+    content: {},
   });
 }
+
+module.exports.cadastrar = (req, res) => {
+  const { password, confirm_password } = req.body;
+  // equivale
+  // const password = req.body.password;
+  // const confirm_password = req.body.confirm_password;
+  if (password !== confirm_password) {
+    res.render('cadastro', {
+      error: {
+        senha: 'Senhas incompativeis',
+      },
+      content: req.body,
+    });
+  }
+  res.send(req.body);
+};
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const login = require('../controller/controller');
+const {
+    login,
+    showCadastro,
+    cadastrar,
+ } = require('../controller/controller');
 
-router.get('/', login.login);
-router.get('/cadastro', login.cadastrar);
+router.get('/', login);
+router.get('/cadastro', showCadastro);
+router.post('/cadastrar', cadastrar)
 
 module.exports = router;
