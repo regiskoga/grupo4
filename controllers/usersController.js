d1f2c455e43e601eca162c1b5337136fe76b16a6
Tradrek <tradrek@gmail.com>
2021-07-21 17:46:09 -0300

    aaa

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,26 @@
-const models = require('../models')
+const models = require('../models');
+const bcrypt = require('bcrypt');
+const moment = require('moment')
+moment.locale('pt-BR')
+//const salt = bcrypt.genSaltSync(10);
 
 module.exports.createUsers = (async (req, res) => {
     const users = req.body
+    const salt = await bcrypt.genSalt(10)
+    users.password = await bcrypt.hash(users.password,salt)
+    console.log(users)
     await models.Users.create(users)
-    //res.send(201)
-    res.redirect('/inscritos')
+    res.status(201).redirect('/inscritos')
 })
 
 module.exports.createUsersPage = (async (req, res) => {
     res.render('newUser')
+})
+
+module.exports.showUserById = (async (req, res) => {
+    const userId = parseInt(req.params.id)
+    const results = await models.Users.findAll({ where: { id: userId } })
+    res.render('newUser', {results, moment:moment})
 })
 
 
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const usersController = require('../controllers/usersController')
 
 router.post('/', usersController.createUsers);
 router.get('/', usersController.createUsersPage);
+router.get('/:id',usersController.showUserById);
 
 module.exports = router;
